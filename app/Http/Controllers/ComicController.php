7a5eae478ffce2a6d9e6a365a20620f0e0fb139a
Riccardo <riccardorocco2001@gmail.com>
2024-02-01 11:24:24 +0100

    completato crud

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -5,15 +5,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Comic;
 
 class ComicController extends Controller
+
 {
+
+    public function validation($data)
+    {
+        return Validator::make($data, [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'thumb' => 'required|url',
+            'price' => 'required|numeric',
+            'series' => 'required',
+            'sale_date' => 'required|date',
+            'type' => 'required',
+        ])->validate();
+    }
+
     public function index()
-{
-    $comics = Comic::all();
-    return view('comics.index', compact('comics'));
-}
+    {
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
+    }
 
 
     public function show($id)
@@ -22,12 +38,29 @@ class ComicController extends Controller
         return view('comics.show', compact('comic'));
     }
 
+    public function create()
+    {
+        return view("comics.create");
+    }
+
+
 
 
     public function edit(Comic $comic)
     {
         return view('comics.edit', compact('comic'));
     }
+
+    public function update(Request $request, Comic $comic)
+    {
+        $data = $request->all();
+        $dati_validati = $this->validation($data);
+
+        $comic->update($dati_validati);
+
+        return redirect()->route('comics.show', $comic->id);
+    }
+
 
     public function search(Request $request)
     {
